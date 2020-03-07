690533e9688a6f5a0281653bd1d797e84049eda1
Natanael Saymon <nsaymonsouza@gmail.com>
2020-03-07 10:57:23 -0300

    inserindo pasta de carousel usando slick

---
+++ Carousel-Slick/js/main.js
@@ -0,0 +1,16 @@
+// $('.slider-principal').slick({
+//     dots: true,
+//     infinite: true,
+//     speed: 300,
+//     slidesToShow: 1,
+//     adaptiveHeight: true,
+//     autoplay: true,
+//     autoplaySpeed: 4000
+// });
+
+$('.carousel').slick({
+    dots: true,
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 4
+});
