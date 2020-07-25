b308871fb453e9f6a356843c2a6ec37e5f41d304
Natanael Saymon <nsaymonsouza@gmail.com>
2020-07-25 18:32:11 -0300

    novas modificações

--- Slide-btn-js/script.js
+++ Slide-btn-js/script.js
@@ -1,4 +1,3 @@
-
 /* Função para pre-carregar as imagens */
 let imgs = []
 let slide
@@ -7,44 +6,43 @@ let maxImage
 let tempoTroca
 let vtempo
 
-function preCarregamento(){
+function preCarregamento() {
   let img = 1
-  for(let i = 0; i < 3; i++){
+  for (let i = 0; i < 3; i++) {
     imgs[i] = new Image()
-    imgs[i].src="image/img"+img+".jpg"
+    imgs[i].src = "image/img" + img + ".jpg"
     img++
   }
 }
 
-function carregaImg(img){
-  slider.style.backgroundImage = "url('"+imgs[img].src+"')"
+function carregaImg(img) {
+  slider.style.backgroundImage = "url('" + imgs[img].src + "')"
   slider.style.backgroundPosition = "center"
   slider.style.backgroundRepeat = "no-repeat"
   slider.style.backgroundSize = "cover"
 }
 
-function troca(direcao){
+function troca(direcao) {
   tempoTroca = 0
-  imgAtual+= direcao
-  if(imgAtual > maxImage){
+  imgAtual += direcao
+  if (imgAtual > maxImage) {
     imgAtual = 0
-  }
-  else if(imgAtual < 0){
+  } else if (imgAtual < 0) {
     imgAtual = maxImage
   }
   carregaImg(imgAtual)
 }
 
-function anima(){
+function anima() {
   tempoTroca++
-  if(tempoTroca >= 500){
+  if (tempoTroca >= 500) {
     tempoTroca = 0
     troca(1)
   }
   window.requestAnimationFrame(anima)
 }
 
-function inicia(){
+function inicia() {
   preCarregamento()
   imgAtual = 0
   maxImage = imgs.length - 1
