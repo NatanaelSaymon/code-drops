8850c0df5740f4c47f17e6782928f7e12bd674ae
Natanael Saymon <saymoneo@gmail.com>
2021-06-03 11:46:03 -0300

    Restruturando projeto

--- projeto_base/gulpfile.js
+++ projeto_base/gulpfile.js
@@ -1,28 +1,17 @@
-
-'use strict';
-
-const gulp = require('gulp');
-const rename = require('gulp-rename')
-const sass = require('gulp-sass'); 
-sass.compiler = require("node-sass"); //necessario para funcionar gulp-sass
-const cssnano = require('gulp-cssnano');
-const autoprefixer = require('gulp-autoprefixer');
-
-gulp.task('default', watch);
-
-gulp.task('sass', compilaSass);
+const gulp = require('gulp')
+const { watch } = require('gulp')
+const sass = require('gulp-sass')
+const uglifycss = require('gulp-uglifycss')
+const concat = require('gulp-concat')
 
 function compilaSass(){
-    return gulp
-        .src("src/scss/**/*.scss")
-        .pipe(sass({outputStyle:'compressed'}).on('error', sass.logError)) //converter sass para css com gulp-sass
-        .pipe(autoprefixer())
-        .pipe(cssnano())
-        .pipe(rename({extname:'.min.css'})) //renomeando arquivo
-        .pipe(gulp.dest("src/css/")); //CSS mimificado com gulp-sass
+    return gulp.src('src/scss/**/*.scss')//filtrando os arquivos que vamos precisar
+        .pipe(sass().on('error', sass.logError))
+        .pipe(uglifycss({ "ugliComments": true }))//pega todo o codigo para ser minificado
+        .pipe(concat('styles.min.css'))//pega todos os arquivos que ja foram compilados e concatena e gera um arquivo no final
+        .pipe(gulp.dest('src/css'))//esse pipe vamos dizer qual destino iremos querer que o codigo fique
 }
 
-function watch(){
-    gulp.watch("src/scss/**/*.scss", compilaSass)
+exports.default = function(){
+    watch('src/scss/**/*.scss', compilaSass)
 }
-
